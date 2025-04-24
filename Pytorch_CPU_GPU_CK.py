fbfd6e8f437c77fd36ed9bd8e67b024709a3dbfa
chh4031 <57095947+chh4031@users.noreply.github.com>
2025-04-24 15:01:31 +0900

    Add files via upload

---
+++ Pytorch_CPU_GPU_CK.py
@@ -0,0 +1,10 @@
+import torch
+print(torch.__version__)
+
+if torch.cuda.is_available():
+    device = torch.device("cuda")
+    print("Using GPU:", torch.cuda.get_device_name(0))
+else:
+    device = torch.device("cpu")
+    print("Using CPU")
+
